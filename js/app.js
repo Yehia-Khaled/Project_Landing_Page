a6808b57bb9a48dc11d6109e268e600ec70c54d9
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-02-22 21:12:06 +0200

    create and style navbar dynamic

--- js/app.js
+++ js/app.js
@@ -36,7 +36,16 @@
 */
 
 // build the nav
+const sections_length= document.querySelectorAll("section").length
+const virtualpage = document.createDocumentFragment();
+for(let i=1;i<=sections_length;i++)
+{
+    const new_section = document.createElement('li');
+    new_section.textContent = 'Section ' + i;
 
+    virtualpage.appendChild(new_section);
+}
+document.querySelector("#navbar__list").appendChild(virtualpage)
 
 // Add class 'active' to section when near top of viewport
 

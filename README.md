9cf42dd6d92672c50e85d124253e68d464b28d39
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-02-23 02:46:39 +0200

    edit js file

--- README.md
+++ README.md
@@ -11,3 +11,6 @@ The starter project has some HTML and CSS styling to display a static version of
 To get started, open `js/app.js` and start building out the app's functionality
 
 For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+## How it work
+
+##advantage of project
--- js/app.js
+++ js/app.js
@@ -19,7 +19,7 @@
  * Define Global Variables
  * 
 */
-
+var listSections=document.querySelectorAll("section");
 
 /**
  * End Global Variables
@@ -31,26 +31,59 @@
 
 /**
  * End Helper Functions
+ * create clean active class
+ * create nav bar fun ,don't forget call it
  * Begin Main Functions
  * 
 */
 
 // build the nav
     //build nav dynamically
+/*
 const sections_length= document.querySelectorAll("section").length
 const virtualpage = document.createDocumentFragment();
 for(let i=1;i<=sections_length;i++)
 {
     const list_section = document.createElement('li');
-    list_section.innerHTML = '<a href="#"> Section </a>'+i;
+    //list_section.innerHTML = '<a href="#"> Section </a>'+i;
+    const newurl=document.createElement('a')
+    newurl.textContent="Section "+i;
+    //list_section.textContent=newurl.getAttribute('data-nav')
+    list_section.appendChild(newurl);
     //list_section.textContent = 'Section ' + i;
 
     virtualpage.appendChild(list_section);
 }
 document.querySelector("#navbar__list").appendChild(virtualpage)
-
+*/
+//call fun
+const virtualpage = document.createDocumentFragment();
+// const listSections=document.querySelectorAll("section");
+listSections.forEach(element=>{
+    const listitems=document.createElement("li");
+    const linkitems=document.createElement("a");
+    linkitems.textContent=element.getAttribute("data-nav");
+    listitems.appendChild(linkitems);
+    virtualpage.appendChild(listitems);
+})
+document.querySelector("#navbar__list").appendChild(virtualpage)
 // Add class 'active' to section when near top of viewport
+function  activeviewport ()
+{//change landing--container to section
+    const activediv=document.getElementsByTagName("section")
+    const rectactive =activediv.getBoundingClientRect()
+    if(rectactive.top>0 && rectactive.top<250)
+    {
+        // create clear function
+        //call clear func
+        /*listSections.forEach(element=> {
+            document.querySelectorAll("section").classList.remove("your-active-class");
 
+
+        })*/
+        // rectactive.classList.add("your-active-class")
+    }
+}
 
 // Scroll to anchor ID using scrollTO event
 

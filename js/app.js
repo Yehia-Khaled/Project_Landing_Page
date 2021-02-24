b3990e754263c6918363404c00729b5c59dceebb
Yehia Khaled <55062155+Yehia-Khaled@users.noreply.github.com>
2021-02-24 12:20:26 +0200

    style nav bar to be accessible in all devices

--- js/app.js
+++ js/app.js
@@ -103,7 +103,17 @@ activeviewport();
 */
 //start Events
 //scroll event
+//i am use cee property to make scroll smooth i don't need to add there too
 document.addEventListener("scroll", activeviewport)
+//create nav event click,us udacity tips
+const linkitems=document.querySelectorAll("a");
+linkitems.forEach(element=>
+{
+    element.addEventListener("click", function () {
+         activeviewport();
+    })
+})
+//edit css style to be flex nav menu (container no element)
 
 //crate event to button to go top page
 //event when click button
@@ -122,79 +132,6 @@ buttonevent.addEventListener("scroll", function() {
      buttonevent.style.display="none";
 })
 
-
-
-
-//code Archive
-/*
-*
-//build nav dynamically
-const sections_length= document.querySelectorAll("section").length
-const virtualpage = document.createDocumentFragment();
-for(let i=1;i<=sections_length;i++)
-{
-    const list_section = document.createElement('li');
-    //list_section.innerHTML = '<a href="#"> Section </a>'+i;
-    const newurl=document.createElement('a')
-    newurl.textContent="Section "+i;
-    //list_section.textContent=newurl.getAttribute('data-nav')
-    list_section.appendChild(newurl);
-    //list_section.textContent = 'Section ' + i;
-
-    virtualpage.appendChild(list_section);
-}
-document.querySelector("#navbar__list").appendChild(virtualpage)
-*
-// Scroll to anchor ID using scrollTO event
-
-//scroll page event
-//  document.addEventListener(scroll,function ()
-//  {
-//      clearactivesection();
-//      activeviewport();
-//  })
-
-*         //Add EventListener to the links and use scrollIntoView().
-       /* const linkevent =document.querySelectorAll("a");
-        linkevent.forEach(element=>
-        {
-            element.addEventListener('scroll', function () {
-                console.log('The nav section was scroll!');
-                element.scrollIntoView();
-            })
-        })
-
-        //activ viewport function
-function  activeviewport ()
-{//change landing--container to section
-    //const activediv=document.getElementsByTagName("section")
-        //if statement to get the Active class
-        listSections.forEach(element=>
-        {
-            /*if(getrect(element))
-            {
-                //element.classList.toggle("your-active-class")
-               // const elementcontain=
-                if(!element.classList.contains("your-active-class"))
-                {
-                    element.classList.add("your-active-class");
-                }
-            }
-
-if(getrect(element) >0 && getrect(element) <200)
-{
-    clearactivesection();
-    element.classList.add("your-active-class");
-
-}
-
-})
-  //function but in event lestener scroll
-function scrollfunction()
-{
-
-}
- */
 //end code here
 //thanks you for review
 
